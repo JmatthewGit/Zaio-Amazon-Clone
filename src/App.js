286f4dff5e48299cc04cf6175d30bda40d6e6247
John-Matthew Ferreira <jmatthewf97@gmail.com>
2025-01-27 15:07:37 +0200

    revert changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
-import { useContext, useEffect } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import Products from "./components/Products";
-import Header from "./components/layout/Header";
+import Header from "./components/Layout/Header";
 import ProductDetails from "./components/ProductDetails";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
 import ShoppingContext from "./context/shopping/shoppingContext";
+import { useContext, useEffect } from "react";
 import { auth } from "./firebase";
+import Checkout from "./components/Checkout";
+import Payment from "./components/Payment";
+
+const promise = loadStripe(
+  'pk_test_51QSekSJgTLpyxgelSkunCA5ad99f6lxOol1zUBluIhIryX6q7IiochvU5SBg2dm3PAgeBErXtLkqRJ1kFQkiQlPX00Zyvz1NB7'
+);
 
 const App = () => {
   const shoppingContext = useContext(ShoppingContext);
   const { setUser } = shoppingContext;
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      console.log("User is -> ", authUser);
+      console.log("User is  ->", authUser);
 
       if (authUser) {
         setUser(authUser);
@@ -24,13 +33,14 @@ const App = () => {
       }
     });
   }, []);
+
   return (
     <>
       <Header />
       <main>
         <Switch>
           <Route path="/" exact>
-            <Redirect to="/home" />
+            <Redirect to="/home"></Redirect>
           </Route>
           <Route path="/home">
             <Home />
@@ -40,6 +50,14 @@ const App = () => {
           </Route>
           <Route path="/products/:id">
             <ProductDetails />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/login">
             <Login />
